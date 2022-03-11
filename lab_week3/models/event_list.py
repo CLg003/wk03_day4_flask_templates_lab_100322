0604546e9640030d2fab28c7d985cd19e7cf977b
CLg003 <clairemjlaing@gmail.com>
2022-03-11 12:29:48 +0000

    updated layout and style, cannot set css background-image

--- lab_week3/models/event_list.py
+++ lab_week3/models/event_list.py
@@ -2,9 +2,9 @@ from asyncio import events
 from xmlrpc.client import FastUnmarshaller
 from models.event import *
 
-event_1 = Event("12/04/1564","Meeting", 8, "Meeting room 1","Party plans",True)
-event_2 = Event("23/08/2012","Festival", 100, "Main Stadium","Spring Break",False)
-event_3 = Event("12/12/2022","Birthday", 4, "Bar","Emma birthday", True)
+event_1 = Event("12/04/2022","Meeting", 8, "Meeting room 1","Party planning meeting",True)
+event_2 = Event("23/08/2022","Festival", 100, "Main Stadium","Summer music festival",False)
+event_3 = Event("12/12/2022","Birthday", 4, "Bar","Emma's 30th birthday", True)
 
 events=[event_1, event_2,event_3]
 
